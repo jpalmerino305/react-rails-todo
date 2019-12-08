e18fbe3c750c3467525795eb3e1569839b59b33d
Paul <jp.almerino305@gmail.com>
2019-12-08 13:13:37 +0000

    Fix attempt for CSS module issue in production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+const merge = require('webpack-merge')
+
+const myCssLoaderOptions = {
+  modules: {
+    localIdentName: '[name]__[local]___[hash:base64:5]'
+  },
+  sourceMap: true,
+}
+
+const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
+
+CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
+
 module.exports = environment
